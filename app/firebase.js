e1510b7dccc2840b5126926cd0b0222f30592a8b
Milanjiji <milanjiji0987654321@gmail.com>
2024-11-14 21:49:18 +0530

    initial commit, header and hero section complete with firebase integrtion for sending email

---
+++ app/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, serverTimestamp } from 'firebase/firestore';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBVdTECNPVWilgl004_BEoIPeNRZy7ue2E",
+
+    authDomain: "reciprokel.firebaseapp.com",
+
+    projectId: "reciprokel",
+
+    storageBucket: "reciprokel.firebasestorage.app",
+
+    messagingSenderId: "229742685890",
+
+    appId: "1:229742685890:web:0d34df8f79bee78019d55b",
+
+    measurementId: "G-Y3W32QD1Y2"
+
+  };
+
+const app = initializeApp(firebaseConfig);
+
+  // Initialize Firestore
+const db = getFirestore(app);
+  
+export { db, serverTimestamp };
