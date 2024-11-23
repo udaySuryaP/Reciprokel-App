4249bf0beaabc7b3b67730facad8865e2ad60c28
Milanjiji <milanjiji0987654321@gmail.com>
2024-11-23 20:53:30 +0530

    we are error

--- app/routes/cmsoon.tsx
+++ app/routes/cmsoon.tsx
@@ -16,7 +16,7 @@ const cmsoon = () =>{
         <body className='cmsoon-container' >
             <p>We’re <br className='cmsoon-break' /> Working on It.</p>
             <Link to='/' style={{display:state.from == 'index' ? 'none' :'flex'}} ><img src="/Arrow3.png" alt="" /></Link>
-            <button onClick={()=>{navigate('/')}}  > <img src="/Arrow3.png" alt="" /></button>
+            <button onClick={()=>{navigate('/')}} style={{display:state.from == 'index' ? 'flex' :'none'}} > <img src="/Arrow3.png" alt="" /></button>
         </body>
     )
 }
